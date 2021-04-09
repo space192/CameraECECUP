9cfdacec36c81e0b04f322c7ab5744b41834c021
Arthur Fournier <44359684+space192@users.noreply.github.com>
2021-04-09 11:57:12 +0200

    Update install_motioneye.sh

--- install_motioneye.sh
+++ install_motioneye.sh
@@ -3,6 +3,8 @@ if [ "$EUID" -ne 0 ]
   then echo "Lancer le programme en administrateur"
   exit
 fi
+apt-get update
+apt-get upgrade -y
 apt-get install ffmpeg libmariadb3 libpq5 libmicrohttpd12 python-pip python-dev libssl-dev libcurl4-openssl-dev libjpeg-dev libz-dev -y
 wget https://github.com/Motion-Project/motion/releases/download/release-4.2.2/pi_buster_motion_4.2.2-1_armhf.deb
 dpkg -i pi_buster_motion_4.2.2-1_armhf.deb
